40ecc7e589f0d04bef9a8f20887759435c43d053
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-25 22:05:24 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,8 +1,11 @@
 class Puppy
-  attr_accessor :name, :breed, :months_old, :age
 
-  def initialize(puppy_attributes)
-    puppy_attributes.each {|key, value| self.send(("#{key}="), value)}
-    months_old = age
-  end
+    attr_accessor :name, :breed, :months_old
+
+    def initialize(name,breed,months_old)
+    @name = name
+    @breed = breed
+    @months_old= months_old
+    end
+
 end
