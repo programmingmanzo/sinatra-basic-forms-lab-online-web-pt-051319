19b0e05f7d47ba4d834bd8d73d20c3fb27c6192b
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-25 21:42:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
+  post '/new' do
     @new_pooch = Puppy.new(params[:name], params[:breed], params[:months_old])
 
     erb :display_puppy
