8f6e4cc70c4e2eaeb892c26cf1bcd9518999e0ff
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-25 22:07:00 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class Puppy
   attr_accessor :name, :breed, :months_old, :age
 
   def initialize(puppy_attributes)
     puppy_attributes.each {|key, value| self.send(("#{key}="), value)}
     months_old = age
+    binding.pry 
   end
 end
